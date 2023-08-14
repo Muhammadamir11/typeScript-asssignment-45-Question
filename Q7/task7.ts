c1ff27a51ce6a7052c4c67864ecc8bd3c171ba75
Muhammadamir11 <142209559+Muhammadamir11@users.noreply.github.com>
2023-08-14 08:07:43 -0700

    task7-completed

---
+++ Q7/task7.ts
@@ -0,0 +1,7 @@
+//Q7
+console.log(`Addition result: ${4+4}`);
+console.log(`Subtraction result: ${12-4}`);
+console.log(`Multiplication result: ${4*2}`);
+console.log(`Division result: ${16/2}`);
+
+
