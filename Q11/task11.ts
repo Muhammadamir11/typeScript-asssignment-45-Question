b3b19f5bc6aae37b9b94f3c911a095dc23de1e47
Muhammadamir11 <142209559+Muhammadamir11@users.noreply.github.com>
2023-08-14 08:30:03 -0700

    task11-completed

---
+++ Q11/task11.ts
@@ -0,0 +1,6 @@
+let names = ["Abdullah","Abubakar",'Umer','Usman','Ali'];
+console.log(`How are you ${names[0]}`);
+console.log(`How are you ${names[1]}`);
+console.log(`How are you ${names[2]}`);
+console.log(`How are you ${names[3]}`);
+console.log(`How are you ${names[4]}`);
