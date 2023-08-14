6db935286109435edd7e72a4bf6053df335edaab
Muhammadamir11 <142209559+Muhammadamir11@users.noreply.github.com>
2023-08-14 10:27:09 -0700

    task13-completed

---
+++ Q13/task13.ts
@@ -0,0 +1,4 @@
+let favoriteTransport = ['Honda motorCycle','smart watch','laptop']
+for (let i=0; i< favoriteTransport.length; i++){
+    console.log(`I would love to own ${favoriteTransport[i]}`);
+}
