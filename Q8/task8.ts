ab4478225fcaa131abec0384a8d99f5a1e2393df
Muhammadamir11 <142209559+Muhammadamir11@users.noreply.github.com>
2023-08-14 08:12:45 -0700

    task8-completed

---
+++ Q8/task8.ts
@@ -0,0 +1,10 @@
+let addition = 4+4;
+let subtraction = 12-4;
+let multiplication = 8*1;
+let division = 16/2;
+console.log("addition: ",addition);
+console.log("subtraction: ",subtraction);
+console.log("multiplication: ",multiplication);
+console.log("division: ",division);
+
+
