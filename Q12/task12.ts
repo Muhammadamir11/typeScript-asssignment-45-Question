136696c759033120119eec70ceb97151b3cf93c9
Muhammadamir11 <142209559+Muhammadamir11@users.noreply.github.com>
2023-08-14 08:32:52 -0700

    task12-completed

---
+++ Q12/task12.ts
@@ -0,0 +1,6 @@
+let names = ["Abdullah,Abubakar,Umer,Usman,Ali"];
+console.log(`Thanks for coming ${names[0]}`);
+console.log(`Thanks for coming ${names[1]}`);
+console.log(`Thanks for coming ${names[2]}`);
+console.log(`Thanks for coming ${names[3]}`);
+console.log(`Thanks for coming ${names[4]}`);
