6eebe7289e7c3e455dbee635f2645eb2842ea06f
Muhammadamir11 <142209559+Muhammadamir11@users.noreply.github.com>
2023-08-14 07:54:41 -0700

    task4-completed

---
+++ Q4/task4.ts
@@ -0,0 +1,2 @@
+let famousPersonality = "Muhammad (SAW)";
+console.log(`${famousPersonality} once said, "It is better to speak to a seeker of knowledge than to remain silent, but silence is better than idle words”`);
